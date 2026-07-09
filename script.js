34165bbd2252de45acf11b1a3eddba83297fd201
BeleCaro <belenacosta84@gmail.com>
2026-07-09 16:14:52 -0300

    Agregar formulario de contacto y estilos asociados

--- script.js
+++ script.js
@@ -88,3 +88,46 @@ document.querySelectorAll('nav a').forEach(function(link) {
     }
   });
 });
+
+// ── FORMULARIO DE CONTACTO ───────────────────────────────────────
+const formulario = document.getElementById("formFake");
+const respuesta = document.getElementById("respuesta");
+
+formulario.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const link = document.getElementById("link").value;
+    const red = document.getElementById("redSocial").value;
+    const descripcion = document.getElementById("descripcion").value;
+
+    if(link === "" || red === "" || descripcion === ""){
+
+        respuesta.innerHTML = `
+            <p class="error">
+                ⚠️ Completá todos los campos obligatorios.
+            </p>
+        `;
+     } else{
+
+    respuesta.innerHTML = `
+        <div class="mensaje-ok">
+            <h3>✅ ¡Gracias por compartir esta publicación!</h3>
+
+            <p>
+            Antes de compartir cualquier noticia recordá:
+            </p>
+
+            <ul>
+                <li>✔ Verificar la fuente.</li>
+                <li>✔ Buscar si otros medios hablan del tema.</li>
+                <li>✔ Leer la nota completa.</li>
+                <li>✔ Revisar la fecha de publicación.</li>
+            </ul>
+        </div>
+    `;
+
+    formulario.reset();
+}
+    
+});
